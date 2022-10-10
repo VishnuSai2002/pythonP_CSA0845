692880afd0708d20d0ca221d4db484882a5ff79d
VishnuSai2002 <112846517+VishnuSai2002@users.noreply.github.com>
2022-10-10 10:30:56 +0530

    Add files via upload

---
+++ day615program.py
@@ -0,0 +1,11 @@
+num = int(input("enter the number:"))
+result = 0
+for i in range(1,num):
+    if(num%i)==	0:
+        result = result+i
+if result==num:
+    print(num,"it is a perfect number")
+
+else:
+     print(num,"it is not a perfect number")
+
